b75749b6d7ed325d0f2ec69d5bb482b53806bd64
Philipp Bauch <philipp.bauch@kinexon.com>
2020-02-17 16:44:24 +0100

    Add basic login component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { App, Login } from "./pages";
+import * as serviceWorker from "./serviceWorker";
+import "./styles/main.scss";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root: React.FunctionComponent = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login" exact={true}>
+          <Login></Login>
+        </Route>
+        <Route path="">
+          <App />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/Login.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Login: React.FunctionComponent = () => {
+  return <div>Login Page</div>;
+};
