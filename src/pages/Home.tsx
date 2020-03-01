81ddf826cbf4d7e76baf31f179a6c48937a64702
Philipp Bauch <ga53xex@mytum.de>
2020-03-01 22:40:33 +0100

    Update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,32 @@
 import React from "react";
-import { Divider, Level } from "../components";
+import { Divider, Level, Tile, Icon } from "../components";
 
 export const Home: React.FunctionComponent = () => {
   return (
     <div id="home-page">
-      <div className="rooms-header">
+      <div className="overview-header">
         <Level>
-          <h1>Rooms</h1>
+          <h1>Übersicht</h1>
         </Level>
         <Divider />
+      </div>
+      <div className="overview-body">
+        <Tile className="overview-tile" dark={true}>
+          <Icon icon="fas fa-th-large" size="lg" />
+          <span className="overview-tile-title">Räume</span>
+        </Tile>
+        <Tile className="overview-tile" dark={true}>
+          <Icon icon="fas fa-mobile-alt" size="lg" />
+          <span className="overview-tile-title">Alle Geräte</span>
+        </Tile>
+        <Tile className="overview-tile" dark={true}>
+          <Icon icon="fas fa-users" size="lg" />
+          <span className="overview-tile-title">Bewohner</span>
+        </Tile>
+        <Tile className="overview-tile" dark={true}>
+          <Icon icon="fas fa-random" size="lg" />
+          <span className="overview-tile-title">Regeln</span>
+        </Tile>
       </div>
     </div>
   );
