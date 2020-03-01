df0844bd24ea098e869caed0b62d0998ffdb8a3e
Philipp Bauch <ga53xex@mytum.de>
2020-03-01 11:11:44 +0100

    Remove unused files

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import { App } from "./App";
 import { PrivateRoute } from "./components/PrivateRoute";
 import { UserProvider } from "./contexts/UserContext";
 import { Login } from "./pages";
-import * as serviceWorker from "./serviceWorker";
 import "./styles/main.scss";
 
 const Root: React.FunctionComponent = () => {
@@ -22,8 +21,3 @@ const Root: React.FunctionComponent = () => {
 };
 
 ReactDOM.render(<Root />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
