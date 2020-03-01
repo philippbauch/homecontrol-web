2059995385aaf000672cf907a6f60337aaf60163
Philipp Bauch <ga53xex@mytum.de>
2020-03-01 22:56:32 +0100

    Add Notifications tab

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,16 +16,20 @@ export const Home: React.FunctionComponent = () => {
           <span className="overview-tile-title">Räume</span>
         </Tile>
         <Tile className="overview-tile" dark={true}>
-          <Icon icon="fas fa-mobile-alt" size="lg" />
-          <span className="overview-tile-title">Alle Geräte</span>
-        </Tile>
-        <Tile className="overview-tile" dark={true}>
           <Icon icon="fas fa-users" size="lg" />
           <span className="overview-tile-title">Bewohner</span>
         </Tile>
         <Tile className="overview-tile" dark={true}>
+          <Icon icon="fas fa-envelope-open-text" size="lg" />
+          <span className="overview-tile-title">Benachrichtigungen</span>
+        </Tile>
+        <Tile className="overview-tile" dark={true}>
           <Icon icon="fas fa-random" size="lg" />
           <span className="overview-tile-title">Regeln</span>
+        </Tile>
+        <Tile className="overview-tile" dark={true}>
+          <Icon icon="fas fa-mobile-alt" size="lg" />
+          <span className="overview-tile-title">Alle Geräte</span>
         </Tile>
       </div>
     </div>
