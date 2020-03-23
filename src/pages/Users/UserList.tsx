89cbf792e7eca72f38771145ce58b45d10033c5d
Philipp Bauch <ga53xex@mytum.de>
2020-03-23 20:38:24 +0100

    Protect /users route

--- src/pages/Users/UserList.tsx
+++ src/pages/Users/UserList.tsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import { UserItem } from "./UserItem";
+import { Loader } from "../../components";
+import { client } from "../../api/client";
 
-interface UserListProps {
-  users: any[];
-}
+export const UserList: React.FunctionComponent = () => {
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState<any[]>([]);
 
-export const UserList: React.FunctionComponent<UserListProps> = ({ users }) => {
+  const fetchUsers = useCallback(async () => {
+    setLoading(true);
+
+    try {
+      const users = await client.get(`/users`);
+
+      setUsers(users);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
+
   return (
-    <div className="user-list">
-      {users.length ? (
-        users.map(user => <UserItem user={user} key={user._id} />)
-      ) : (
-        <span>No users found.</span>
-      )}
-    </div>
+    <Loader loading={loading}>
+      <div className="user-list">
+        {users.length ? (
+          users.map(user => <UserItem user={user} key={user._id} />)
+        ) : (
+          <span>No users found.</span>
+        )}
+      </div>
+    </Loader>
   );
 };
--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,42 +1,31 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { Link, Route, Switch } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, Route, Switch, Redirect } from "react-router-dom";
 import { UserList } from "./UserList";
-import { client } from "../../api/client";
-import { Loader } from "../../components";
 import { Page } from "../../layout";
 import { AddUser } from "../AddUser";
+import { UserContext } from "../../contexts/UserContext";
 
 export const Users: React.FunctionComponent = () => {
-  const [loading, setLoading] = useState(true);
-  const [users, setUsers] = useState([]);
+  const { user } = useContext(UserContext);
+
+  const getDefaultRoute = () => {
+    const { activeHomeId } = user.preferences;
+
+    return activeHomeId ? `/homes/${activeHomeId}` : "/homes";
+  };
+
+  if (!user.admin) {
+    return <Redirect to={getDefaultRoute()} />;
+  }
 
   const action = <Link to={`/users/new`}>Erstellen</Link>;
-
-  const fetchUsers = useCallback(async () => {
-    setLoading(true);
-
-    try {
-      const users = await client.get(`/users`);
-
-      setUsers(users);
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-    }
-  }, []);
-
-  useEffect(() => {
-    fetchUsers();
-  }, [fetchUsers]);
 
   return (
     <Switch>
       <Route component={AddUser} path={`/users/new`} />
       <Route>
         <Page action={action} title="Benutzer">
-          <Loader loading={loading}>
-            <UserList users={users} />
-          </Loader>
+          <UserList />
         </Page>
       </Route>
     </Switch>
