2ae69c7942d8ac1c3bf3ce798590a2179679274c
Philipp Bauch <ga53xex@mytum.de>
2020-04-18 15:55:16 +0200

    Add forwardRef to Tile component

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -2,6 +2,7 @@ import classnames from "classnames";
 import React from "react";
 
 interface TileProps {
+  children?: React.ReactNode;
   className?: string;
   darker?: boolean;
   id?: string;
@@ -9,25 +10,21 @@ interface TileProps {
   padded?: boolean;
 }
 
-export const Tile: React.FunctionComponent<TileProps> = ({
-  children,
-  className,
-  darker,
-  id,
-  onClick,
-  padded = true
-}) => {
-  return (
-    <div
-      className={classnames("tile", className, {
-        "is-clickable": !!onClick,
-        "is-darker": darker,
-        "is-padded": padded
-      })}
-      id={id}
-      onClick={onClick}
-    >
-      {children}
-    </div>
-  );
-};
+export const Tile = React.forwardRef<HTMLDivElement, TileProps>(
+  ({ children, className, darker, id, onClick, padded = true }, ref) => {
+    return (
+      <div
+        className={classnames("tile", className, {
+          "is-clickable": !!onClick,
+          "is-darker": darker,
+          "is-padded": padded,
+        })}
+        id={id}
+        onClick={onClick}
+        ref={ref}
+      >
+        {children}
+      </div>
+    );
+  }
+);
