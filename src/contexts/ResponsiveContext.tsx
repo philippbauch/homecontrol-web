350b828d27659fd48a6d35406e03f09884e3dab9
Philipp Bauch <ga53xex@mytum.de>
2020-05-12 10:24:45 +0200

    Add ResponsiveContext

---
+++ src/contexts/ResponsiveContext.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+
+type ScreenSize = "mobile" | "tablet" | "desktop" | "large" | undefined;
+
+const SCREEN_TABLET = 768;
+const SCREEN_DESKTOP = 1024;
+const SCREEN_LARGE = 1366;
+
+const ResponsiveStateContext = React.createContext<any | undefined>(undefined);
+
+const ResponsiveProvider: React.FunctionComponent = ({ children }) => {
+  const [screenSize, setScreenSize] = useState<ScreenSize>();
+
+  const recordScreenSize = () => {
+    const width = window.innerWidth;
+
+    if (width >= SCREEN_LARGE) {
+      setScreenSize("large");
+    } else if (width >= SCREEN_DESKTOP) {
+      setScreenSize("desktop");
+    } else if (width >= SCREEN_TABLET) {
+      setScreenSize("tablet");
+    } else {
+      setScreenSize("mobile");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", recordScreenSize);
+
+    recordScreenSize();
+
+    return () => window.removeEventListener("resize", recordScreenSize);
+  }, []);
+
+  return (
+    <ResponsiveStateContext.Provider
+      value={{
+        screenSize,
+        isScreenMobile: () => screenSize === "mobile",
+        isScreenTablet: () => screenSize === "tablet",
+        isScreenDesktop: () => screenSize === "desktop",
+        isScreenLarge: () => screenSize === "large",
+      }}
+    >
+      {children}
+    </ResponsiveStateContext.Provider>
+  );
+};
+
+function useResponsiveState() {
+  const context = React.useContext(ResponsiveStateContext);
+
+  if (context === undefined) {
+    throw new Error(
+      "useResponsiveState must be used within a ResponsiveStateContext"
+    );
+  }
+
+  return context;
+}
+
+export { ResponsiveProvider, useResponsiveState };
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { App } from "./App";
+import { ResponsiveProvider } from "./contexts/ResponsiveContext";
 import { UserProvider } from "./contexts/UserContext";
 import { Login } from "./pages";
 import "./styles/main.scss";
@@ -9,12 +10,14 @@ import "./styles/main.scss";
 const Root: React.FunctionComponent = () => {
   return (
     <Router>
-      <UserProvider>
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route component={App} />
-        </Switch>
-      </UserProvider>
+      <ResponsiveProvider>
+        <UserProvider>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route component={App} />
+          </Switch>
+        </UserProvider>
+      </ResponsiveProvider>
     </Router>
   );
 };
