da112c13956769ac2fbdc544ba3d74394a46d659
Philipp Bauch <ga53xex@mytum.de>
2020-05-25 23:18:23 +0200

    Display mock data on Courses page

--- src/pages/Courses/Courses.tsx
+++ src/pages/Courses/Courses.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Button, Stack, Card, Tab, Tabs, UserIcon } from "../../components";
-import { Page } from "../../layout";
-import { useUserState } from "../../contexts/UserContext";
 import { useHistory } from "react-router-dom";
+import { Button, Stack, Card, Tab, Tabs, UserIcon } from "../../components";
+import { useCoursesState } from "../../contexts/CoursesContext";
+import { Page } from "../../layout";
 
 export const Courses: React.FunctionComponent = () => {
-  const user = useUserState();
+  const { courses } = useCoursesState();
   const history = useHistory();
 
   return (
@@ -18,97 +18,18 @@ export const Courses: React.FunctionComponent = () => {
         <Tab>Favoriten</Tab>
       </Tabs>
       <Stack gap="lg" vertical={true}>
-        <Card
-          className="courses-item"
-          onClick={() => history.push("/courses/1")}
-        >
-          <UserIcon user={user} />
-          <div>
-            <h3>Mobile verteilte Systeme</h3>
-            <p>
-              In diesem Kurs lernen wir etwas über verteilte Systeme. Das wird
-              mit Sicherheit ein riesen Spaß!
-            </p>
-          </div>
-        </Card>
-        <Card
-          className="courses-item"
-          onClick={() => history.push("/courses/2")}
-        >
-          <UserIcon user={user} />
-          <div>
-            <h3>Security Engineering</h3>
-            <p>
-              In diesem Kurs lernen wir etwas über verteilte Systeme. Das wird
-              mit Sicherheit ein riesen Spaß!
-            </p>
-          </div>
-        </Card>
-        <Card
-          className="courses-item"
-          onClick={() => history.push("/courses/3")}
-        >
-          <UserIcon user={user} />
-          <div>
-            <h3>Diskrete Wahrscheinlichkeitstheorie</h3>
-            <p>
-              In diesem Kurs lernen wir etwas über verteilte Systeme. Das wird
-              mit Sicherheit ein riesen Spaß!
-            </p>
-          </div>
-        </Card>
-        <Card
-          className="courses-item"
-          onClick={() => history.push("/courses/3")}
-        >
-          <UserIcon user={user} />
-          <div>
-            <h3>Diskrete Wahrscheinlichkeitstheorie</h3>
-            <p>
-              In diesem Kurs lernen wir etwas über verteilte Systeme. Das wird
-              mit Sicherheit ein riesen Spaß!
-            </p>
-          </div>
-        </Card>
-        <Card
-          className="courses-item"
-          onClick={() => history.push("/courses/3")}
-        >
-          <UserIcon user={user} />
-          <div>
-            <h3>Diskrete Wahrscheinlichkeitstheorie</h3>
-            <p>
-              In diesem Kurs lernen wir etwas über verteilte Systeme. Das wird
-              mit Sicherheit ein riesen Spaß!
-            </p>
-          </div>
-        </Card>
-        <Card
-          className="courses-item"
-          onClick={() => history.push("/courses/3")}
-        >
-          <UserIcon user={user} />
-          <div>
-            <h3>Diskrete Wahrscheinlichkeitstheorie</h3>
-            <p>
-              In diesem Kurs lernen wir etwas über verteilte Systeme. Das wird
-              mit Sicherheit ein riesen Spaß!
-            </p>
-          </div>
-        </Card>
-        <Card
-          className="courses-item"
-          onClick={() => history.push("/courses/3")}
-        >
-          <UserIcon user={user} />
-          <div>
-            <h3>Diskrete Wahrscheinlichkeitstheorie</h3>
-            <p>
-              In diesem Kurs lernen wir etwas über verteilte Systeme. Das wird
-              mit Sicherheit ein riesen Spaß!
-            </p>
-          </div>
-        </Card>
+        {courses.map((course) => (
+          <Card
+            className="courses-item"
+            onClick={() => history.push(`/courses/${course.id}`)}
+          >
+            <UserIcon user={{ identifier: course.title }} />
+            <div>
+              <h3>{course.title}</h3>
+              <p>{course.description}</p>
+            </div>
+          </Card>
+        ))}
       </Stack>
     </Page>
   );
